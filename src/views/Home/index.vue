e6d91ab169f5c32a192b6ab70becacf2d3c2ca9b
Hassan Haidar <hassan.haidar@mail.mcgill.ca>
2020-02-19 19:27:55 -0500

    Populate homepage

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -5,7 +5,22 @@
     <br />
     <div class="title grey--text">
       Welcome to the TechGames Backend Challenge!
+      <br />
+      <br />
     </div>
+    <div id="mission">
+    <h3 class="display-1"> Mission </h3>
+    <p class="body-1">Your mission is simple: implement features in the website's backend server. Of course,
+    since the success of the website is of the highest importance, we expect you to make use of all the best practices:
+    Frequent and relevent commits
+    Test driven development
+    Flexible architecture
+    Respect of the SOLID principles
+    etc.
+    <br />
+You have 6 hours to complete the most features while respecting high development standards. Are you ready?
+</p>
+    </div> 
   </v-container>
 </template>
 
